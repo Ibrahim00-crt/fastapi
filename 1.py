51381fa5ff2064be70f86157d3954ea81cda0486
Ibrahim00-crt <83007351+Ibrahim00-crt@users.noreply.github.com>
2024-09-15 02:44:38 -0700

    1.py

--- 1.py
+++ 1.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-app.add_middleware(
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Permettre toutes les origines (à restreindre en production)
